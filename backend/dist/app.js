890efed1264d18a2c964a70b411590f23de46671
Roodev19 <randyodoom19@>
2024-01-15 06:45:31 +0000

    setting up node enviroment

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -4,5 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cluster_1 = __importDefault(require("cluster"));
+const os_1 = __importDefault(require("os"));
 require("dotenv/config");
 const app = (0, express_1.default)();
+// CLUSTERING
+const numCPUs = os_1.default.cpus().length;
+if (cluster_1.default.isPrimary) {
+    console.log(`Master process is running with process ID ${process.pid}`);
+    // Fork worker processes
+    for (let i = 0; i < numCPUs; i++) {
+        cluster_1.default.fork();
+    }
+    // Handle process termination
+    cluster_1.default.on('exit', (worker, code, signal) => {
+        console.log(`Worker process ${worker.process.pid} exited with code ${code} and signal ${signal}`);
+        console.log('Forking a new worker process...');
+        cluster_1.default.fork();
+    });
+}
+else {
+    const port = process.env.PORT || 3003;
+    app.listen(port, () => {
+        console.log(`Worker process ${process.pid} listening on port ${port}`);
+    });
+}
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,3 +5,29 @@ import 'dotenv/config';
 import bodyParser from 'body-parser';
 
 const app = express();
+
+// CLUSTERING
+const numCPUs = os.cpus().length;
+
+if (cluster.isPrimary) {
+    console.log(`Master process is running with process ID ${process.pid}`);
+
+    // Fork worker processes
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    // Handle process termination
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(
+            `Worker process ${worker.process.pid} exited with code ${code} and signal ${signal}`
+        );
+        console.log('Forking a new worker process...');
+        cluster.fork();
+    });
+} else {
+    const port = process.env.PORT || 3003;
+    app.listen(port, () => {
+        console.log(`Worker process ${process.pid} listening on port ${port}`);
+    });
+}
